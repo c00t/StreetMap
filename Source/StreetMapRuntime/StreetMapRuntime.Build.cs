973b3fcaa7bfa13c071d42501455f91aca18c102
c0t <38101941+c00t@users.noreply.github.com>
2018-12-01 17:32:36 +0800

    Update StreetMapRuntime.Build.cs

--- Source/StreetMapRuntime/StreetMapRuntime.Build.cs
+++ Source/StreetMapRuntime/StreetMapRuntime.Build.cs
@@ -7,6 +7,8 @@ namespace UnrealBuildTool.Rules
         public StreetMapRuntime(ReadOnlyTargetRules Target)
 			: base(Target)
 		{
+		            PCHUsage = ModuleRules.PCHUsageMode.NoSharedPCHs;
+		            PrivatePCHHeaderFile = "StreetMapRuntime.h";
 			PrivateDependencyModuleNames.AddRange(
 				new string[] {
                     "Core",
